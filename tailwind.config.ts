de37c3dbf7c27207b88006319680e24b67e2fe87
frbarbre <fr.barbre@gmail.com>
2023-08-10 15:47:21 +0200

    ready to style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        violet: "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        gray: "hsl(0, 0%, 75%)",
+        "gray-violet": "hsl(257, 7%, 63%)",
+        "dark-blue": "hsl(255, 11%, 22%)",
+        "dark-voilet": "hsl(260, 8%, 14%)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
