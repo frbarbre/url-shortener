186eb57dda09eea6f9fe1c683b959e634730f643
frbarbre <fr.barbre@gmail.com>
2023-08-12 03:56:15 +0200

    final commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
       transition={{ delay: 1 }}
       className="px-[24px] md:px-[48px] bg-dark-violet py-[54px] md:py-[71px]"
     >
-      <m.section initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0}} className="flex flex-col lg:flex-row items-center justify-between max-w-[1110px] mx-auto lg:items-start gap-[48px]">
+      <m.section initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1.2}} className="flex flex-col lg:flex-row items-center justify-between max-w-[1110px] mx-auto lg:items-start gap-[48px]">
         <Image src={"/logo-white.svg"} alt="logo" width={120} height={32.6} />
         <article className="flex flex-col md:flex-row gap-y-[40px] gap-x-[78px] text-center md:text-left">
           {footerLinks.map((link) => (
